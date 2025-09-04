213dc2c5e1d306ad94075eb225507a81f79d34dd
ismi50 <imis23fy@student.ju.se>
2025-09-04 13:58:46 +0200

    started on the second version of my "flow of life". I added a flow to the line so it looks kind of like "neon" and more "alive"

--- experiments/second.js
+++ experiments/second.js
@@ -1,1 +1,29 @@
+let t = 0;
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noFill();
+}
+
+function draw() {
+  background(0, 5);
+  drawFlow(100);
+  t += 0.005;
+}
+
+function drawFlow(index) {
+  let baseY = height / 2;
+  stroke(255, 255, 0);
+  strokeWeight(2);
+  beginShape();
+  for (let x = 0; x < width; x += 2) {
+    let y = baseY + map(noise(x * 0.005, t), 0, 1, -100, 100);
+    vertex(x, y);
+  }
+  endShape();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(0);
+}
